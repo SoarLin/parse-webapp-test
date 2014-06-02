1496f84cf515a2a1d1473d7ae1a26f71a4f86c9b
soar <soar.lin@gmail.com>
2014-06-02 17:27:13 +0800

    office location vote v0.1

--- vote/js/app.js
+++ vote/js/app.js
@@ -1,23 +1,198 @@
+//Parse JavaScript Key : fVd6fGuWnpkrGY2voBbS3jp8vDCmlplAKaPONWvX
+
 (function() {
+/*
+  Parse.initialize("wvKilDh5ZK8ODgHaslQf7xBoiGnX2RWhMmC1GJfq", "fVd6fGuWnpkrGY2voBbS3jp8vDCmlplAKaPONWvX");
+
+  var RoomObject = Parse.Object.extend("RoomObject");
+  var p_room = new RoomObject();
+  var p_query = new Parse.Query(RoomObject);
+  var p_data = {};
+
+  // $.fn.getRoomList = function(){
+  function getRoomList(){
+    p_room.fetch({
+      success : function(data){
+        p_data = data.toJSON();
+      },
+      error : function(roomObject, error){
+        alert("Error: "+error.description);
+      }
+    });
+  }
+
+  //getRoomList();
+  //console.log(p_data);
+
+  function saveToParse(newRoom){
+    p_room.set("link", newRoom.link);
+    p_room.set("cost", newRoom.cost);
+    p_room.set("bed", newRoom.bed);
+    p_room.set("register", newRoom.register);
+    p_room.set("points", newRoom.points);
+    p_room.save(null, {
+      success : function(room){
+        alert("save success! need to refresh table, room id = "+room.id);
+      },
+      error : function(room, error){
+        alert("Error: "+error.description);
+      }
+    });
+  };
+
+  var parse = angular.module('LocationVoteService', []);
+  parse.factory('ParseService', function(){
+    Parse.initialize("wvKilDh5ZK8ODgHaslQf7xBoiGnX2RWhMmC1GJfq", "fVd6fGuWnpkrGY2voBbS3jp8vDCmlplAKaPONWvX");
+
+    var RoomObject = Parse.Object.extend("RoomObject");
+    var p_room = new RoomObject();
+
+    var ParseService = {
+      name: "Parse",
+
+      getRoomList : function getRoomList(callback){
+        p_room.fetch({
+          success : function(data){
+            callback(data.toJSON());
+          },
+          error : function(error){
+            alert("Error: "+error.description);
+          }
+        });
+      },
+
+      saveToParse : function saveToParse(newRoom, callback){
+        p_room.set("link", newRoom.link);
+        p_room.set("cost", newRoom.cost);
+        p_room.set("bed", newRoom.bed);
+        p_room.set("register", newRoom.register);
+        p_room.set("points", newRoom.points);
+        p_room.save(null, {
+          success : function(room){
+            alert("save success! need to refresh table, room id = "+room.id);
+            //callback(room);
+          },
+          error : function(room, error){
+            alert("Error: "+error.description);
+          }
+        });
+      }
+    }
+
+    return ParseService;
+  });
+*/
+
   var app = angular.module('LocationVote', []);
-  app.controller('VoteController', function(){
-    this.products = rooms;
-    
+
+  app.factory('ParseService', function(){
+    Parse.initialize("wvKilDh5ZK8ODgHaslQf7xBoiGnX2RWhMmC1GJfq", "fVd6fGuWnpkrGY2voBbS3jp8vDCmlplAKaPONWvX");
+
+    var RoomObject = Parse.Object.extend("RoomObject");
+    var p_query = new Parse.Query(RoomObject);
+    var p_room = new RoomObject();
+
+    var ParseService = {
+      name: "Parse",
+
+      getRoomList : function getRoomList(callback){
+        p_room.fetch({
+          success : function(data){
+            var json = data.toJSON()
+            // console.log(json.results);
+            callback(json.results);
+          },
+          error : function(error){
+            alert("Error: "+error.description);
+          }
+        });
+      },
+
+      saveToParse : function saveToParse(newRoom, callback){
+        p_room.set("link", newRoom.link);
+        p_room.set("cost", newRoom.cost);
+        p_room.set("traffic", newRoom.traffic);
+        p_room.set("bed", newRoom.bed);
+        p_room.set("register", newRoom.register);
+        p_room.set("points", newRoom.points);
+        p_room.save(null, {
+          success : function(room){
+            //allRooms.push(newRoom);
+            //alert("save success! need to refresh table, room id = "+room.id);
+            callback(room.toJSON());
+          },
+          error : function(room, error){
+            alert("Error: "+error.message);
+          }
+        });
+      },
+
+      countPoints: function countPoints(newPoint, room){
+        console.log("I am ParseService, newPoint = "+newPoint);
+        p_query.equalTo("traffic", room.traffic);
+        p_query.find({
+          success:function(room) {
+            // list contains post liked by the current user which have the title "I'm Hungry".
+            // console.log(room[0]);
+            room[0].set("points", newPoint);
+            room[0].save();
+          },
+          error : function(error) {
+            alert("Error: "+error.message);
+          }
+        });
+      }
+    }
+
+    return ParseService;
+  });
+
+  app.controller("AddRoomController", function($scope, ParseService){
+    this.newRoom = {};
+
+    this.addNewRoom = function(rooms){
+      this.newRoom.points = 0;
+      
+      ParseService.saveToParse(this.newRoom, function(results){
+        $scope.$apply(function() {
+          rooms.push(results);
+          // console.log(results);
+        });
+      });
+
+      this.newRoom = {};
+    };
+  });
+
+  app.controller('VoteController', function($scope, ParseService){
+    var voteCtrl = this;
+    voteCtrl.rooms = [];
+
+    // console.log($scope);
+    ParseService.getRoomList(function(results){
+      $scope.$apply(function() {
+        voteCtrl.rooms = results;
+      });
+    });
+
     this.canRegister = function(reg){
-      if (reg === true) {  return "可以"; } 
-      else if (reg === false) {  return "不行"; }
+      if (reg === 'true') {  return "可以"; } 
+      else if (reg === 'false') {  return "不行"; }
       else {  return "未知"; }
     };
     
     this.countPoints = function(vote, room){
+      var newPoint = vote;
       if(room.points !== 0)
-        room.points = (vote + room.points)/2;
-      else
-        room.points = vote;
+        newPoint = (vote + room.points)/2;
+
+      ParseService.countPoints(newPoint, room);
+      room.points = newPoint;
     };
+
   });
-  
-  var rooms = [
+
+  var OfficeRooms = [
     {
       link: "http://www.google.com",
       cost: 23000,
